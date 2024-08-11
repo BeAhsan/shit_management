cffaa52bd3d83ae9b4d7923bd8793143c9e881b7
ahsan <bahsan035@gmail.com>
2024-08-11 15:18:02 +0100

    dummy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
 
         $this->roleData();
         $this->createAdmin();
+        $this->createDummy();
         User::factory(10)->withPersonalTeam()->create();
         // User::factory()->withPersonalTeam()->create([
         //     'name' => 'Test User',
@@ -43,7 +44,19 @@ class DatabaseSeeder extends Seeder
             'email'=> 'admin@gmail.com',
             'email_verified_at' => now(),
             'role_id'=> 1,
+            'active'=> 1,
             'password'=>  Hash::make('admin123')
+        ]);
+    }
+
+    function createDummy() : void {
+        User::create([
+            'name' => 'Test',
+            'email'=> 'test@gmail.com',
+            'email_verified_at' => now(),
+            'role_id'=> w,
+            'active'=> 1,
+            'password'=>  Hash::make('test123')
         ]);
     }
 
