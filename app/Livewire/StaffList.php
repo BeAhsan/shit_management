4362d09fe39870f8897e367d5b37889b1ae461b2
ahsan <bahsan035@gmail.com>
2024-08-11 19:21:25 +0100

    search

--- app/Livewire/StaffList.php
+++ app/Livewire/StaffList.php
@@ -5,21 +5,32 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
-use Request;
+use Illuminate\Http\Request;
 
 class StaffList extends Component
 {
+    public $name = '';
+    public $searchedUser = [];
     public function render()
     {
         return view('livewire.staff-list');
     }
 
-    function search(Request $request){
-        dd($request);
-    }
 
-    public function getAll() {
-      return  User::where('role_id', 2)
+ 
+
+    public function getUsers() {
+
+
+       if ($this->name) {
+       
+       return User::where('name','LIKE','%'.$this->name.'%')
+       ->orWhere('staff_number','LIKE','%'.$this->name.'%')
+       ->orWhere('email','LIKE','%'.$this->name.'%')->paginate(8);
+      
+    } 
+
+       return User::where('role_id', 2)
         ->paginate(
             $perPage = 8, $columns = ['*'], $pageName = 'users'
         
--- resources/views/livewire/staff-list.blade.php
+++ resources/views/livewire/staff-list.blade.php
@@ -1,12 +1,9 @@
 <div>
-    <h1>List</h1>
-
   
-        <form wire:submit.prevent="search" class="flex gap-3 w-1/2" method="post">
-            <div>
-                <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+        <form wire:submit.prevent="getUsers" class="flex gap-3 w-1/2 mb-12" method="post">
+        @csrf 
+                <x-input id="name" class="block mt-1 w-full" placeholder="Search By Name" type="text" name="name"  wire:model="name" :value="old('name')" required autofocus autocomplete="name" />
+            
             <x-button class="ms-4">
                     {{ __('Search') }}
                 </x-button>
@@ -29,7 +26,7 @@
             </th>
         </thead>
         <tbody>
-        @foreach($this->getAll() as $user)
+        @foreach(($this->getUsers()) as $user)
          <tr class="border-b-2 h-12">
             <td>{{$user->staff_number == null ? '-': $user->staff_number}}</td> 
             <td>{{$user->name}}</td> 
@@ -40,7 +37,7 @@
         </tbody>
     </table>
     <div class=" mt-4">
-    {{ $this->getAll()->links() }}
+    {{ $this->getUsers()->links() }}
     </div>
    
 </div>
